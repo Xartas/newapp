6cf283def3188791b009f936e91943f2f749bcbf
Mateusz Wieczorek <mateusz.wieczorek@anytimegames.pl>
2021-09-27 11:38:55 +0200

    Sprzątanie

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
     <div>Tutaj będzie jakieś coś</div>
   );
 }
-
-export default App;
