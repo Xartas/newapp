7d43b963f29bbe6d33dfc0a5bdc1faf91388b382
Sebastian Wieczorek <sebastian.wieczorek1989@gmail.com>
2021-09-27 13:07:12 +0200

    Poprawka w App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-import './App.css';
-
-function App() {
-=======
 export default function App() {
->>>>>>> 5a6c5f2c4532fcee17e4df7b8fc32e415bebf900
   return (
     <div>Tutaj będzie jakieś coś</div>
   );
